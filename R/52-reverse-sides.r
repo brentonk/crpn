316d1a7bafd2a097141b5494d2ea33d0d6c2a724
Brenton Kenkel <brenton.kenkel@gmail.com>
2015-06-02 09:33:24 -0500

    Clean up calculation of side-reversed probs

--- R/52-reverse-sides.r
+++ R/52-reverse-sides.r
@@ -12,17 +12,27 @@ source("26-full-formula.r")
 source("40-load-results.r")
 
 ## Construct "reversed" data
-idx_a <- which(names(work_train) %in% paste0("SideA_", cinc_components))
-idx_b <- which(names(work_train) %in% paste0("SideB_", cinc_components))
-work_train_rev <- work_train
-names(work_train_rev)[idx_a] <- str_replace(names(work_train_rev)[idx_a],
-                                            "SideA",
-                                            "SideB")
-names(work_train_rev)[idx_b] <- str_replace(names(work_train_rev)[idx_b],
-                                            "SideB",
-                                            "SideA")
+reverse_sides <- function(dat)
+{
+    ## Column indices of side-specific variables
+    idx_a <- str_detect(names(dat), fixed("SideA_"))
+    idx_b <- str_detect(names(dat), fixed("SideB_"))
 
-## Calculate predicted probabilities
+    ## Switch 'em
+    names(dat)[idx_a] <- str_replace(names(dat)[idx_a],
+                                     "SideA",
+                                     "SideB")
+    names(dat)[idx_b] <- str_replace(names(dat)[idx_b],
+                                     "SideB",
+                                     "SideA")
+
+    dat
+}
+
+work_train_rev <- reverse_sides(work_train)
+work_test_rev <- reverse_sides(work_test)
+
+## Calculate predicted probabilities for training data
 pp_orig <- predict(fit_rf_time,
                    newdata = work_train,
                    type = "prob")
@@ -34,3 +44,15 @@ pp_rev <- pp_rev[, c(3, 2, 1)]
 ## Compare predicted probabilities (median absolute difference)
 pp_diff <- abs(pp_orig - pp_rev)
 apply(pp_diff, 2, median)
+
+## Same for test data
+pp_test_orig <- predict(fit_rf_time,
+                        newdata = work_test,
+                        type = "prob")
+pp_test_rev <- predict(fit_rf_time,
+                       newdata = work_test_rev,
+                       type = "prob")
+pp_test_rev <- pp_test_rev[, c(3, 2, 1)]
+
+pp_test_diff <- abs(pp_test_orig - pp_test_rev)
+apply(pp_test_diff, 2, median)
