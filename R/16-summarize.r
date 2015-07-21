0f421bbe1647aca52f635a17ff7ddfba6e048f8b
Brenton Kenkel <brenton.kenkel@gmail.com>
2015-07-20 21:54:23 -0500

    Add plots of ensemble results for slides

--- R/16-summarize.r
+++ R/16-summarize.r
@@ -6,8 +6,14 @@
 ################################################################################
 
 library("dplyr")
+library("ggplot2")
+library("RColorBrewer")
+library("scales")
+library("tikzDevice")
 library("xtable")
 library("yaml")
+
+Sys.unsetenv("TEXINPUTS")
 
 load("results-data-nmc.rda")
 load("results-imputations-train.rda")
@@ -76,28 +82,28 @@ model_table %>% group_by(data) %>% summarise(logLoss = mean(logLoss))
 model_table %>% group_by(year) %>% summarise(logLoss = mean(logLoss))
 
 ## Prettify numeric values and truncate small ensemble weights
-model_table <- mutate(model_table,
-                      logLoss = sprintf("%.2f", logLoss),
-                      prl = sprintf("%.2f", prl),
-                      prl = gsub("-", "$-$", prl, fixed = TRUE),
-                      weight = ifelse(weight > 0.005,
-                                      sprintf("%.2f", weight),
-                                      "$<$0.01"))
+model_xtable <- mutate(model_table,
+                       logLoss = sprintf("%.2f", logLoss),
+                       prl = sprintf("%.2f", prl),
+                       prl = gsub("-", "$-$", prl, fixed = TRUE),
+                       weight = ifelse(weight > 0.005,
+                                       sprintf("%.2f", weight),
+                                       "$<$0.01"))
 
 ## Use checkmarks for whether year is included
 ##
 ## Requires amssymb in the LaTeX file
-model_table <- mutate(model_table,
-                      year = ifelse(year,
-                                    "$\\checkmark$",
-                                    ""))
+model_xtable <- mutate(model_xtable,
+                       year = ifelse(year,
+                                     "$\\checkmark$",
+                                     ""))
 
 ## Blank irrelevant values
-model_table[1, "prl"] <- ""
-model_table[nrow(model_table), c("data", "year", "weight")] <- ""
+model_xtable[1, "prl"] <- ""
+model_xtable[nrow(model_xtable), c("data", "year", "weight")] <- ""
 
 ## Convert to LaTeX
-model_xtable <- model_table %>%
+model_xtable <- model_xtable %>%
     select(-name) %>%
     xtable(align = c("l", "l", "l", "c", "r", "r", "r"))
 colnames(model_xtable) <- c("Method",
@@ -114,6 +120,133 @@ print(model_xtable,
       include.rownames = FALSE,
       sanitize.text.function = identity,
       hline.after = c(-1, 0, nrow(model_xtable) - 1, nrow(model_xtable)))
+
+
+###-----------------------------------------------------------------------------
+### Plot ensemble results for slides
+###-----------------------------------------------------------------------------
+
+plot_data <- model_table
+
+## Parameters common across plots
+tikz_width <- 4.25
+tikz_height <- 3.25
+tikz_size <- 8
+tikz_theme <-
+    theme(plot.background = element_rect(fill = "transparent", colour = NA),
+          legend.background = element_rect(fill = "transparent", colour = NA))
+tikz_package = c(getOption("tikzLatexPackages"),
+                 "\\usepackage{amsmath}",
+                 "\\usepackage{amssymb}")
+
+## Histogram of ensemble weights
+##
+## WARNING: Manually annotated, not robust to changes in the ensemble
+tikz(file = file.path("..", "slides", "fig-weight-hist.tex"),
+     width = tikz_width,
+     height = tikz_height)
+print(
+    plot_data %>%
+        filter(name != "ensemble") %>%
+        ggplot(aes(x = weight)) +
+        geom_histogram(binwidth = 0.01) +
+        annotate("text",
+                 x = 0.2,
+                 y = 2.5,
+                 label = "Neural Network\n(Proportions + Year)",
+                 size = 3) +
+        annotate("segment",
+                 x = 0.265,
+                 xend = 0.325,
+                 y = 2.5,
+                 yend = 1) +
+        annotate("text",
+                 x = 0.275,
+                 y = 7.5,
+                 label = "Random Forest\n(Components + Year)",
+                 size = 3) +
+        annotate("segment",
+                 x = 0.275,
+                 xend = 0.355,
+                 y = 6.2,
+                 yend = 1.1) +
+        scale_x_continuous("Super Learner Weight") +
+        scale_y_continuous("Count") +
+        theme_grey(base_size = tikz_size) +
+        tikz_theme
+)
+dev.off()
+
+## Relationship between PRL and ensemble weight
+tikz(file = file.path("..", "slides", "fig-weight-by-cvl.tex"),
+     width = tikz_width,
+     height = tikz_height,
+     package = tikz_package)
+print(
+    plot_data %>%
+        filter(name != "ensemble") %>%
+        ggplot(aes(x = logLoss, y = weight)) +
+        geom_point() +
+        stat_smooth(se = FALSE,
+                    method = "loess") +
+        scale_y_log10("Ensemble Weight") +
+        scale_x_continuous("CV Loss") +
+        theme_grey(base_size = tikz_size) +
+        tikz_theme
+)
+dev.off()
+
+## Total ensemble weight across each model type
+tikz(file = file.path("..", "slides", "fig-weight-by-method.tex"),
+     width = tikz_width,
+     height = 0.55 * tikz_height,
+     package = tikz_package)
+print(
+    plot_data %>%
+        filter(name != "ensemble") %>%
+        group_by(method) %>%
+        summarise(weight = sum(weight)) %>%
+        arrange(desc(weight)) %>%
+        mutate(method = factor(method,
+                               levels = rev(method))) %>%
+        ggplot(aes(x = method, y = weight)) +
+        geom_bar(stat = "identity") +
+        scale_x_discrete("") +
+        scale_y_continuous("") +
+        ggtitle("Total Weight by Method") +
+        coord_flip() +
+        theme_grey(base_size = tikz_size) +
+        tikz_theme
+)
+dev.off()
+
+## Total ensemble weight across each data source
+tikz(file = file.path("..", "slides", "fig-weight-by-data.tex"),
+     width = tikz_width,
+     height = 0.5 * tikz_height,
+     package = tikz_package)
+print(
+    plot_data %>%
+        filter(name != "ensemble") %>%
+        mutate(data = ifelse(year,
+                             paste(data, "+", "Year"),
+                             data)) %>%
+        group_by(data) %>%
+        summarise(weight = sum(weight)) %>%
+        arrange(desc(weight)) %>%
+        mutate(data = factor(data,
+                             levels = rev(data),
+                             labels = gsub("only", "", rev(data)))) %>%
+        ggplot(aes(x = data, y = weight)) +
+        geom_bar(stat = "identity") +
+        scale_x_discrete("") +
+        scale_y_continuous("") +
+        ggtitle("Total Weight by Data Source") +
+        coord_flip() +
+        theme_grey(base_size = tikz_size) +
+        tikz_theme
+)
+dev.off()
 
 
 ###-----------------------------------------------------------------------------
