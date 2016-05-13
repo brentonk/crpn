ecbeb0e6c52561f2953b779aba0263eb303f9cab
Brenton Kenkel <brenton.kenkel@gmail.com>
2016-05-13 14:28:38 -0500

    Update Dreyer 2010 replication

--- replications/dreyer-2010.r
+++ replications/dreyer-2010.r
@@ -32,33 +32,43 @@ data_dreyer_2010 <- data_dreyer_2010 %>%
                      year = "year")) %>%
     mutate(VictoryMax = pmax(VictoryA, VictoryB),
            VictoryMin = pmin(VictoryA, VictoryB))
-stopifnot(with(data_dreyer_2010, sum(is.na(VictoryA)) == 0))
+stopifnot(with(data_dreyer_2010, !any(is.na(VictoryA))))
 
-## Reproduce original model
-set.seed(410)
+## Replicating Model 2
 f_dreyer_2010 <-
     mid ~ rap_iss_accum + grad_iss_accum + cap_rat + maj_power + alliance +
         democ + contig + spline_1 + spline_2 + spline_3 + spline_4
+
+## Null hypothesis: no effect of issue accumulation
+hyp_main <- c("rap_iss_accum = 0", "grad_iss_accum = 0")
+
+## Reproduce original model
+set.seed(410)
 cr_dreyer_2010 <- glm_and_cv(
     form = f_dreyer_2010,
     data = data_dreyer_2010,
+    se_cluster = data_dreyer_2010$rivdyad,
+    hyp_main = hyp_main,
+    hyp_power = "cap_rat = 0",
     number = 10,
     repeats = 100
 )
-printCoefmat(cr_dreyer_2010$summary)
+print(cr_dreyer_2010$summary)
 
 ## Replicate, replacing CINC scores with DOE scores
 ##
 ## Logging DOE scores for consistency with original analysis
-set.seed(14)
 doe_dreyer_2010 <- glm_and_cv(
     form = update(f_dreyer_2010,
                   . ~ . - cap_rat + log(VictoryMax) + log(VictoryMin)),
     data = data_dreyer_2010,
+    se_cluster = data_dreyer_2010$rivdyad,
+    hyp_main = hyp_main,
+    hyp_power = c("log(VictoryMax) = 0", "log(VictoryMin) = 0"),
     number = 10,
-    repeats = 100,
+    repeats = 100
 )
-printCoefmat(doe_dreyer_2010$summary)
+print(doe_dreyer_2010$summary)
 
 save(cr_dreyer_2010,
      doe_dreyer_2010,
