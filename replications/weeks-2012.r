6d4903fb5c70657a780db04024aa47dd7be69f2e
Brenton Kenkel <brenton.kenkel@gmail.com>
2016-05-17 09:43:20 -0500

    Update Weeks 2012 replication

--- replications/weeks-2012.r
+++ replications/weeks-2012.r
@@ -34,33 +34,46 @@ data_weeks_2012 <- left_join(data_weeks_2012,
                                     year = "year"))
 stopifnot(with(data_weeks_2012, sum(is.na(VictoryA)) == 0))
 
-## Replicate original model and cross-validate
-set.seed(2312)
-f_weeks_2012 <- 
+## Table 1, Model 2
+f_weeks_2012 <-
     mzinit ~ machinejlw_1 + juntajlw_1 + bossjlw_1 + strongmanjlw_1 +
         allotherauts_1 + newregime_1 + democracy_2 + cap_1 + cap_2 + initshare +
         dependlow + majmaj + minmaj + majmin + contigdum + logdist + s_wt_glo +
         s_lead_1 + s_lead_2 + pcyrsmzinit + pcyrsmzinits1 + pcyrsmzinits2 +
         pcyrsmzinits3
+
+## Null hypothesis: no variation in effects across types of authoritarians
+hyp_main <- c("juntajlw_1 = machinejlw_1",
+              "bossjlw_1 = machinejlw_1",
+              "strongmanjlw_1 = machinejlw_1",
+              "allotherauts_1 = machinejlw_1")
+
+## Replicate original model and cross-validate
+set.seed(2312)
 cr_weeks_2012 <- glm_and_cv(
     form = f_weeks_2012,
     data = data_weeks_2012,
+    se_cluster = data_weeks_2012$dirdyadid,
+    hyp_main = hyp_main,
+    hyp_power = c("initshare = 0", "cap_1 = 0", "cap_2 = 0"),
     number = 10,
     repeats = 10
 )
-printCoefmat(cr_weeks_2012$summary)
+print(cr_weeks_2012$summary)
 
 ## Replicate, replacing all functions of the CINC score (the two raw scores and
 ## the ratio) with DOE scores
-set.seed(2132)
 doe_weeks_2012 <- glm_and_cv(
     form = update(f_weeks_2012,
                   . ~ . - cap_1 - cap_2 - initshare + VictoryA + VictoryB),
     data = data_weeks_2012,
+    se_cluster = data_weeks_2012$dirdyadid,
+    hyp_main = hyp_main,
+    hyp_power = c("VictoryA = 0", "VictoryB = 0"),
     number = 10,
     repeats = 10
 )
-printCoefmat(doe_weeks_2012$summary)
+print(doe_weeks_2012$summary)
 
 save(cr_weeks_2012,
      doe_weeks_2012,
